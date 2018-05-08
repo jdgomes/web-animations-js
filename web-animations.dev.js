2ae52c5442910de273bf1561d67bbdc6c0046277
J G <jgomes@apterainc.com>
2018-05-08 13:16:37 -0400

    Catch document undefined error (Angular Universal)

--- web-animations.dev.js
+++ web-animations.dev.js
@@ -14,8 +14,10 @@
 var webAnimationsSourceTarget = 'web-animations';
 var WEB_ANIMATIONS_TESTING = false;
 (function() {
-  var scripts = document.getElementsByTagName('script');
-  var location = scripts[scripts.length - 1].src.replace(/[^\/]+$/, '');
-  document.write('<script src="' + location + 'target-config.js"></script>');
-  document.write('<script src="' + location + 'target-loader.js"></script>');
+  try{
+    var scripts = document.getElementsByTagName('script');
+    var location = scripts[scripts.length - 1].src.replace(/[^\/]+$/, '');
+    document.write('<script src="' + location + 'target-config.js"></script>');
+    document.write('<script src="' + location + 'target-loader.js"></script>');
+  }catch(e){}
 })();
